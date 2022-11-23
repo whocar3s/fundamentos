94385f31948948c938cabf7a250752645316f21b
valis <valisoho@gmail.com>
2022-11-22 20:46:55 -0500

    a

--- mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -13,6 +13,6 @@ public class Mavenproject1 {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("a");
+        System.out.println("Hello World!");
     }
 }
