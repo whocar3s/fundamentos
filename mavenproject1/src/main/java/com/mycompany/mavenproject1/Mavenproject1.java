d66a94c7d24705b8362a21d0dadc20c8518f7ad5
whocar3s <2paula.paez1@gmail.com>
2022-11-22 21:31:56 -0500

    gol

--- mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -15,5 +15,7 @@ public class Mavenproject1 {
         System.out.println("Hello World!");
         System.out.println("Hello World!");
         System.out.println("Hello World!");
+        
+        System.out.println("gol");
     }
 }
